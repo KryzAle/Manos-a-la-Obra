5ea14a16047e11b0a10ba18b814485fa7bf31184
Cristian Cabezas <kryzale@gmail.com>
2020-06-24 23:25:57 -0500

    Registro Usuario @CCabezas

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:manos_a_la_obra/main.dart';
+import 'package:manos_a_la_obra/src/repository/user_repository.dart';
 
 void main() {
+  final UserRepository user = UserRepository();
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(App(userRepository: user));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
